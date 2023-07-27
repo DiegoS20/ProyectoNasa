b7e68cbd2ca97d0f8ee4f399a89608615b05bfbf
Diego Saravia <saravia__2000@hotmail.com>
2023-07-27 08:24:55 -0600

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import Home from './src/views/Home';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Home />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rgba(7,26,93,255)',
+  },
+});
