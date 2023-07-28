990886b51bd259f8f72db37eb27dfa8e0f4c5c9f
Diego Saravia <saravia__2000@hotmail.com>
2023-07-28 08:07:34 -0600

    feat: router configuration

---
+++ src/routes.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationOptions,
+} from '@react-navigation/native-stack';
+import Home from './views/Home';
+import Detail from './views/Detail';
+
+const Stack = createNativeStackNavigator<RootStackParams>();
+const routeScreenDefaultOptions: NativeStackNavigationOptions = {
+  headerStyle: {
+    backgroundColor: 'rgba(7,26,93,255)',
+  },
+  headerTitleStyle: {
+    color: '#fff',
+  },
+};
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={routeScreenDefaultOptions}
+        />
+        <Stack.Screen
+          name="Detail"
+          component={Detail}
+          options={routeScreenDefaultOptions}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export type RootStackParams = {
+  Home: undefined;
+  Detail: undefined;
+};
